dd2f60d023e56c69eb921a4fc09aa056fff8345f
xaviergriffon <griffon.xav@free.fr>
2019-11-24 22:06:33 +0100

    Ajout tache gradle et indicateurs

---
+++ README.md
@@ -0,0 +1,8 @@
+# BattleDev
+
+## Utilisation
+
+1. Recopiez les entrées dans le fichier : src/main/resources/dataInput.txt
+2. Recopiez les sorties dans le fichier : src/main/resources/dataOutputExpected.txt
+3. Implémentez la solution dans la classe com.isograd.exercise.IsoContest
+4. Lancez le test en exécutant la tâche gradle run ou  depuis l'IDE (pour débugger par ex.) en effectuant un Run de la méthode com.isograd.exercise.RunTestWithData.main
--- src/main/java/com/isograd/exercise/RunTestWithData.java
+++ src/main/java/com/isograd/exercise/RunTestWithData.java
@@ -3,38 +3,117 @@ package com.isograd.exercise;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.util.Calendar;
 import java.util.List;
 
 public class RunTestWithData {
-    public static void main(String[] args) throws Exception {
-        PrintStream originalOut = System.out;
-        MyPrintStream myPrintStream = new MyPrintStream(originalOut, "dataOutputExpected.txt");
-        System.setOut(myPrintStream);
-        InputStream originalIn = System.in;
-        System.setIn(new MyInputStream("dataInput.txt"));
-        IsoContest.main(null);
 
-        System.setOut(originalOut);
-        System.setIn(originalIn);
+    private static final int WARN_TIME = 30;
+    private static final int WARN_MEMORY_MO = 5;
 
-        if (myPrintStream.isAllExpectedLinesProvided()) {
-            System.out.println("Success !");
-        } else {
-            System.out.println("Failed challenge :(");
+    private static final double MEGABYTE = 1024d * 1024d;
+    /*
+     * Palette de couleur pour la console
+     */
+    enum ConsoleColor {
+        ANSI_RESET("\u001B[0m"),
+        ANSI_BLACK("\u001B[30m"),
+        ANSI_RED("\u001B[31m"),
+        ANSI_GREEN("\u001B[32m"),
+        ANSI_YELLOW("\u001B[33m"),
+        ANSI_BLUE("\u001B[34m"),
+        ANSI_PURPLE("\u001B[35m"),
+        ANSI_CYAN("\u001B[36m"),
+        ANSI_WHITE("\u001B[37m");
+        private String stringColor;
+        ConsoleColor(String stringColor) {
+            this.stringColor = stringColor;
+        }
+        public String getStringColor() {
+            return this.stringColor;
         }
     }
 
-    static class MyPrintStream extends PrintStream {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Configuration des Entrées/Sorties pour intercepter les demandes afin de fournir
+         * les entrées de l'exercice et comparer les sorties aux attentes.
+         */
+        PrintStream originalOut = System.out;
+        RunTestPrintStream runTestPrintStream = new RunTestPrintStream(originalOut, "dataOutputExpected.txt");
+        System.setOut(runTestPrintStream);
+        InputStream originalIn = System.in;
+        System.setIn(RunTestWithData.class
+                .getClassLoader()
+                .getResourceAsStream("dataInput.txt"));
+
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        Calendar startTime = Calendar.getInstance();
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        IsoContest.main(null);
+        long usedMemory = (runtime.totalMemory() - runtime.freeMemory()) - memory;
+        Calendar endTime = Calendar.getInstance();
+
+        // Redéfinition des Entrées/Sorties avec leurs valeurs originales.
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+
+        // Indicateurs d'exécutions
+        checkMemoryUsage(usedMemory);
+        checkExecutionTime(endTime.getTimeInMillis() - startTime.getTimeInMillis());
+
+        // Résultat
+        if (runTestPrintStream.isAllExpectedLinesProvided()) {
+            println(ConsoleColor.ANSI_BLUE, "Success !");
+        } else {
+            println(ConsoleColor.ANSI_RED, "Failed challenge :(");
+        }
+    }
+
+    private static void checkExecutionTime(long timeInMillis) {
+        DecimalFormat df = new DecimalFormat("0.##");
+        double timeInSecond = timeInMillis / 1000d;
+        println(ConsoleColor.ANSI_GREEN, "Execution time : " + df.format(timeInSecond) + "s");
+        if (timeInSecond > WARN_TIME) {
+            println(ConsoleColor.ANSI_RED, "/!\\ execution time > " + WARN_TIME + "s");
+        }
+    }
+
+    public static void checkMemoryUsage(double usedMemoryInOctet) {
+        DecimalFormat df = new DecimalFormat("0.##");
+        println(ConsoleColor.ANSI_GREEN, "Used memory : "+(df.format( usedMemoryInOctet/MEGABYTE)) + "Mo");
+        if (usedMemoryInOctet > (WARN_MEMORY_MO * MEGABYTE)) {
+            println(ConsoleColor.ANSI_YELLOW, "/!\\ Memory usage > " + WARN_MEMORY_MO + "Mo");
+        }
+    }
+
+    public static void println(ConsoleColor color, String message) {
+        println(System.out, color, message);
+    }
+
+    public static void println(PrintStream printStream, ConsoleColor color, String message) {
+        printStream.println(color.getStringColor() + message + ConsoleColor.ANSI_RESET.getStringColor());
+    }
+
+    public static void print(PrintStream printStream, ConsoleColor color, String message) {
+        printStream.print(color.getStringColor() + message + ConsoleColor.ANSI_RESET.getStringColor());
+    }
+
+    static class RunTestPrintStream extends PrintStream {
         List<String> allLinesExpected;
         int nExpectedLine;
         int nBadOut;
         boolean allExpectedLinesProvided = false;
 
-        public MyPrintStream(OutputStream out, String dataOuputExpectedFile) throws IOException {
+        public RunTestPrintStream(OutputStream out, String dataOuputExpectedFile) throws IOException {
             super(out);
             allLinesExpected = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(dataOuputExpectedFile).getPath()));
         }
+
+        // --- Toutes méthodes println doivent utiliser println(String x)
 
         @Override
         public void println(int x) {
@@ -51,19 +130,21 @@ public class RunTestWithData {
             super.println(String.valueOf(x));
         }
 
+        // -----------------
+
         @Override
         public void println(String x) {
             String expectedOut = null;
             if (nExpectedLine < allLinesExpected.size()) {
                 expectedOut = allLinesExpected.get(nExpectedLine++);
                 if (!expectedOut.equals(x)) {
-                    super.print("Bad out ! Expected : " + expectedOut + " | ");
+                    RunTestWithData.print((PrintStream) super.out, ConsoleColor.ANSI_RED, "Bad out ! Expected : " + expectedOut + " | ");
                     nBadOut++;
                 } else if (nExpectedLine == allLinesExpected.size() && nBadOut == 0) {
                     allExpectedLinesProvided = true;
                 }
             } else {
-                super.print("No out expected !! => ");
+                RunTestWithData.print((PrintStream) super.out, ConsoleColor.ANSI_RED, "No out expected !! => ");
             }
 
             super.println(x);
@@ -71,22 +152,6 @@ public class RunTestWithData {
 
         public boolean isAllExpectedLinesProvided() {
             return allExpectedLinesProvided;
-        }
-    }
-
-    static class MyInputStream extends InputStream {
-
-        private InputStream inputStreamFile;
-
-        public MyInputStream(String dataFile) throws IOException {
-            inputStreamFile = getClass()
-                    .getClassLoader().getResourceAsStream(dataFile);
-
-        }
-
-        @Override
-        public int read() throws IOException {
-            return inputStreamFile.read();
         }
     }
 }
